f9c52895bdcbf29de927d93cdfa7dec4c7551105
CAT9527 <37021526+CAT9527@users.noreply.github.com>
2019-01-25 09:33:18 +0800

    2048
    1.导入核心类文件 GameCore.cs/Location.cs/MoveDirection.cs
    2.修改GameCore中GenerateNumber()方法，返回位置与数字。
    3.在GameContrller中
      -- 创建GameCore类对象。
      -- 添加GenerateNumber方法，内部调用核心类算法GenerateNumber。
      -- 在生成精灵时，将精灵行为脚本引用存储到二维数组中。

--- UGUIProgram/Assets/Scripts/2048/GameController.cs
+++ UGUIProgram/Assets/Scripts/2048/GameController.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using Console2048;
 public class GameController : MonoBehaviour {
 
     /// <summary>
     /// 2048游戏控制器
     /// </summary>
 
+    private GameCore core;
+    private NumberSprite[,] spriteActionArray;  //精灵行为的二维数组
     private void Start()
     {
+        core = new GameCore();
+        spriteActionArray = new NumberSprite[4,4];
+
         Init();
+
+        GenerateNewNumber();
+        GenerateNewNumber();
     }
 
     private void Init()
@@ -27,16 +35,29 @@ public class GameController : MonoBehaviour {
     }
 
     //创建一个方格Sprite
-    private void CreateSprite(int i,int j)
+    private void CreateSprite(int r,int c)
     {
         //Instantiate();
-        GameObject go = new GameObject(i.ToString()+j.ToString());
+        GameObject go = new GameObject(r.ToString()+c.ToString());
         //设置图片
         go.AddComponent<Image>();
         NumberSprite action = go.AddComponent<NumberSprite>();
+        //将引用存储到二维数组里spriteActionArray
+        spriteActionArray[r, c] = action;
         action.SetImage(0);
         //创建游戏对象，Scale默认为1，设置为false表示相对父物体的坐标
         go.transform.SetParent(this.transform,false);  
 
+    }
+
+    //生成新数字
+    private void GenerateNewNumber()
+    {
+        Location loc;
+        int number;
+        core.GenerateNumber(out loc, out number);
+
+        //根据位置，获取精灵行为脚本对象引用
+        spriteActionArray[loc.RIndex, loc.CIndex].SetImage(number);
     }
 }
